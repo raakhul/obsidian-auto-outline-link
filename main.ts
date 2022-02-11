7792ec92f0110066545ccb3f4550f6405dfa032b
raakhul <91585121+raakhul@users.noreply.github.com>
2022-02-11 20:41:02 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,137 +1,63 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutomaticLinks extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("heading-glyph", "Outline Links", async () => {    
+
+			const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+			const noteFile = this.app.workspace.getActiveFile(); // Currently Open Note
+			if(!noteFile.name) return; // Nothing Open
+
+			const headingsArray = await this.extractHeadings(noteFile);
+
+			const selected=editor.getSelection();
+			editor.replaceSelection(headingsArray.toString(),selected);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	//To Read contents of active file
+	async extractHeadings(noteFile: TFile):  Promise<any[]> {
+		
+		// Read the currently open note file. We are reading it off the HDD - we are NOT accessing the editor to do this.
+		let text = await this.app.vault.cachedRead(noteFile); //To read active cached file
+		let extracted_headings=text.match(/^##[\s].+/gm); //To select lines which has only heading 2
+
+		const headingsArray = extracted_headings.map(headings => {	//To Remove ## from selected headings
+			let hashRemoved=headings.match(/(?![##\s]).+/gm);
+			return hashRemoved;
+		})
+		
+		let headingsLinkArray=this.generateHeadingsLink(headingsArray); //To Generate md links for headings 2
+
+		return headingsLinkArray;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	//To Generate md links for headings 2
+	generateHeadingsLink (headings: RegExpMatchArray[]): any[] {
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		let headingsArray: Array<String>;
+		let headingsLinkArray = new Array();
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		headingsArray = headings.toString().split(",");
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		for (var i=0; i<headingsArray.length;i++) {
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+			let heading: String;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+			//Construting md link by replacing whitespace with /
+			if (i>=1 && i<(headingsArray.length-1)) {
+				heading=" [".concat(headingsArray[i].toString()).concat("](#").concat(headingsArray[i].toString().toLowerCase().replace(/\s/g,"/")).concat(")");
+				headingsLinkArray.push(heading.toString());
+			}
+		}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return headingsLinkArray;
 	}
 }
